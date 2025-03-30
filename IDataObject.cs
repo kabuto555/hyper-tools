9fd0abd8ec7d2577e0aa6c253c8d8db7ede646d0
Clarky Lee <howling.ape@gmail.com>
2025-03-30 14:53:29 -0400

    Add missing IDataObject interface

---
+++ IDataObject.cs
@@ -0,0 +1,7 @@
+namespace HyperTools
+{
+    public interface IDataObject
+    {
+        public string Id { get; set; }
+    }
+}
--- StaticDataRepository.cs
+++ StaticDataRepository.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Model;
 using Newtonsoft.Json;
 using UnityEngine;
 
